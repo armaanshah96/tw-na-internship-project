c34c1e890d05f6a39264aa22a0de177cdc2e109e
AlecW901 <86252784+AlecW901@users.noreply.github.com>
2021-06-30 16:19:43 -0500

    DI/AW-Added Target and Community member user button and prepped test

--- app/src/components/SituationalQuestions.tsx
+++ app/src/components/SituationalQuestions.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import QuestionList from "./QuestionList";
-
+import Button from "@material-ui/core/Button";
+import TableOfContents from "./TableOfContents";
 function SituationalQuestions() {
+    TableOfContents()
     return <div>
         <QuestionList/>
     </div>
--- app/src/components/TableOfContents.tsx
+++ app/src/components/TableOfContents.tsx
@@ -1,10 +1,10 @@
 import React, { useState} from 'react';
 import KnowYourRights from "./KnowYourRights";
+import Button from "@material-ui/core/Button";
 
 
 function TableOfContents(){
     const [showContent, setShowContent] = useState(false)
-
     function clickHandler(){
         setShowContent(true);
     }
@@ -12,6 +12,11 @@ function TableOfContents(){
         <div >
             {(showContent == false)? <button onClick={clickHandler}>Know Your Rights</button>:null}
             {showContent && <KnowYourRights/>}
+            {showContent?
+            <Button variant="contained" color={"primary"} >Targeted User</Button>:null}
+            {showContent?
+                <Button variant="contained" color={"primary"} >Community Member User </Button>:null}
+
         </div>
     )
 }
