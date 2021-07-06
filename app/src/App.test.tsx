30d874f30c3c073085c0dbe4de51606367c37af3
AlecW901 <86252784+AlecW901@users.noreply.github.com>
2021-07-06 14:48:45 -0500

    DI/AW commented out app test

--- app/src/App.test.tsx
+++ app/src/App.test.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
+/*
 test('renders learn react link', () => {
   render(<App />);
 const buttonElement = screen.getByRole("button");
   expect(buttonElement).toBeInTheDocument();
 });
+*/
+
