7d0e4cc9909468cbfc67482721481ec1105af732
Armaan Shah <armaan.shah@thoughtworks.com>
2021-07-15 14:16:39 -0400

    AS - #NO_CARD - Fix typo in frontend checkpoint

--- app/src/services/catService.js
+++ app/src/services/catService.js
@@ -1,7 +1,7 @@
 /*
     Key concepts: Promise + `.then`, HTTP request, Asynchronous code
 
-    CHECKPOINT: what does calling getCatPic() do? What is "fetch" (google: Fetch API) and how does it relate to HTTP and our backend?
+    CHECKPOINT: what does calling getGrumpiestCat() do? What is "fetch" (google: Fetch API) and how does it relate to HTTP and our backend?
     CHECKPOINT: Tricky - why is specifying '/api/cats/grumpiest' enough information? (hint: the backend url, localhost:8080, is specified in react app codebase)
 
     Topics we'll touch on: what is a Promise, what does fetch do
