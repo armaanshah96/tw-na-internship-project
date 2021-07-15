bbad713f09b4917605b891973dc13116469232e6
devonivory <71669240+devonivory@users.noreply.github.com>
2021-07-15 13:12:39 -0400

    DI/AW/AM/JE/MG Adding KYRController,KYRControllerTest,Get request to Rest-api

--- src/main/java/com/example/demo/controller/KnowYourRightsController.java
+++ src/main/java/com/example/demo/controller/KnowYourRightsController.java
@@ -1,27 +1,21 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.KYR;
+import com.example.demo.service.KYRService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class KnowYourRightsController {
-/*    CHALLENGE: (Armaan/Danie will let yall know when to start on this) Can you create an endpoint to get all KnowYourRights content?
+    private final KYRService kyrService;
 
-        Criteria to meet:
-        1. endpoint is available at "api/know-your-rights"
-        2. The HTTP method used is GET
-        3. Follow Controller-Service-Repository structure
-        4. Return all KnowYourRights content from the existing `kyr` table in Heroku
-        5. If you find you have extra time, try introducing tests. While normally we always test, tests are not the main goal of this activity
+    public KnowYourRightsController(KYRService kyrService){
+        this.kyrService=kyrService;
 
-        Hints:
-        1. Feel free to pattern-match from the Cat example if you don't know where to start (see: Follow CatController getAllCats method)
-        2. You will need to create a separate class (and file) for KnowYourRights Service and Repository and Model
-        3. Don't forget the Spring annotations!!! For example: @RestController at the top of a controller file to indicate
-        to Springboot that this controller is intended to be part of our REST API
+    }
+    @GetMapping("/api/know-your-rights")
+    public Iterable<KYR> getAllKYR(){
+        return kyrService.getAllKYR();
+    }
 
-        Goal:
-        - Applying what we've learned about application architecture and REST api to our project
-        - Can you articulate what you figured out that you didn't know prior to this exercise?
-
-        When we come back together (today or tomorrow depending on time):
-        - Mob pair on this task using test driven development
-        - We are going to rotate drivers and main navigator very frequently using "Code with Me"
-*/
 }
