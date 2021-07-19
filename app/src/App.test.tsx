7b1143850d10388977aa944746b3f3d0188886d0
Armaan Shah <armaan.shah@thoughtworks.com>
2021-07-19 11:26:50 -0400

    AS - #NO_CARD - Add review checkpoints to frontend tests

--- app/src/App.test.tsx
+++ app/src/App.test.tsx
@@ -2,7 +2,18 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-   const app = render(<App />);
+/*
+    Key concepts: Snapshot testing (see: jest snapshot testing documentation)
+
+    CHECKPOINT: So far we've predominantly been creating 'unit tests'. What is a snapshot test and what is it comparing?
+    CHECKPOINT: What does this mean for the team in terms of keeping this test up to date?
+
+    Topics to go over: snapshot testing
+
+    Note: while this test is very useful in frontend codebases, we will likely remove it after discussion
+ */
+
+test('renders App component', () => {
+  const app = render(<App />);
   expect(app).toMatchSnapshot();
 });
