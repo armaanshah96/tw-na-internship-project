67ce99acd39ae7ccbcb069a21797b198ddba2740
abdulrahman moussa <abdi@abdulrahmans-MacBook-Pro.local>
2021-07-21 16:31:11 -0500

    AM/MG - 2 - rebased from main

--- app/src/components/TableOfContents.tsx
+++ app/src/components/TableOfContents.tsx
@@ -1,31 +1,19 @@
 import React, { useState} from 'react';
-<<<<<<< HEAD
 import SituationalQuestions from "./SituationalQuestions";
 
 
 function TableOfContents(){
     const [showButton, setShowButton] = useState(false)
-    function clickHandler(){
+    function clickHandler() {
         setShowButton(true);
-=======
-import KnowYourRights from "./KnowYourRights";
+    }
 
-
-function TableOfContents(){
-
-    const [showContent, setShowContent] = useState(false)
-
-    function clickHandler(){
-        setShowContent(true);
 
     return(
         <div >
             {(showButton == false)?  <button onClick={clickHandler}>Know Your Rights</button>:null}
             {showButton && <SituationalQuestions/>}
 
-
-            {/*{(showContent === false)? <button onClick={clickHandler}>Know Your Rights</button>:null}
-            {showContent && <KnowYourRights  />}*/}
 
         </div>
     )
