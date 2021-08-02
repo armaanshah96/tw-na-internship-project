3b37be93fe07857213a215a737369bb48006a865
abdulrahman moussa <abdi@abdulrahmans-MacBook-Pro.local>
2021-08-02 12:02:48 -0500

    AM/AW - 35 - added accordion summary styling and fixed unique key prop error

--- app/src/components/KnowYourRights.js
+++ app/src/components/KnowYourRights.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import knowYourRightsService from "../services/knowYourRightsService";
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import Accordion from '@material-ui/core/Accordion';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
@@ -12,66 +12,67 @@ import {Box} from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
 
-  root: {
-    width: '100%',
-  },
-  heading: {
-    fontSize: theme.typography.pxToRem(15),
-    fontWeight: theme.typography.fontWeightRegular,
-  },
+        root: {
+            width: '100%',
+        },
+        heading: {
+            fontSize: theme.typography.pxToRem(15),
+            fontWeight: theme.typography.fontWeightRegular,
+        },
 
-
-})
+    })
 );
 
 
-
-function KnowYourRights (){
+function KnowYourRights() {
 
     const [knowYourRightsData, setKnowYourRightsData] = useState(undefined)
     const classes = useStyles();
 
     useEffect(() => {
         getKnowYourRightsData();
-      }, []);
+    }, []);
 
-      const getKnowYourRightsData = async () => {
+    const getKnowYourRightsData = async () => {
         const result = await knowYourRightsService.getKnowYourRightsContent();
         setKnowYourRightsData(result);
-      }
-      return (
-                <div data-testid="kyrContent" >
+    }
+    return (
+        <div data-testid="kyrContent">
 
-                    <h1 > Know Your Rights </h1>
-                    {knowYourRightsData?.map((x) => {
-                        return (
-                            <div className={classes.root}>
-                            <Box mx={5} my={2} >
-                                <Accordion >
-                                    <AccordionSummary
-                                        expandIcon={<ExpandMoreIcon/>}
-                                        aria-controls="panel1a-content"
-                                        id="panel1a-header"
-                                    >
-                                        <Typography>Title: {x.title} Summary: {x.summary} </Typography>
-                                    </AccordionSummary >
+            <h1> Know Your Rights </h1>
+            {knowYourRightsData?.map((x) => {
+                return (
+                    <div key={x.id} className={classes.root}>
+                        <Box mx={5} my={2}>
+                            <Accordion>
+                                <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon/>}
+                                    aria-controls="panel1a-content"
+                                    id="panel1a-header"
+                                >
+                                    <h5>{x.title}
+                                        <Typography className={classes.heading}>
+                                            Summary: {x.summary}
+                                        </Typography>
+                                    </h5>
+                                </AccordionSummary>
 
-                                     <AccordionDetails>
-                                        <Typography className={classes.heading}> Additional Info: </Typography>
-                                     </AccordionDetails>
-                                     <AccordionDetails>
-                                        <Typography className={classes.heading}> References: </Typography>
-                                     </AccordionDetails>
-                                </Accordion>
-                            </Box>
-                            </div>
+                                <AccordionDetails>
+                                    <Typography className={classes.heading}> Additional Info: </Typography>
+                                </AccordionDetails>
+                                <AccordionDetails>
+                                    <Typography className={classes.heading}> References: </Typography>
+                                </AccordionDetails>
+                            </Accordion>
+                        </Box>
+                    </div>
+                )
+            })}
+        </div>
 
-
-                        )})}
-                        </div>
-
-                    )}
-
+    )
+}
 
 
 export default KnowYourRights;
