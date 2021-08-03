261c054b3e554f687901c535798a38acd1a9c618
abdulrahman moussa <abdi@abdulrahmans-MacBook-Pro.local>
2021-08-03 16:56:22 -0500

    AM - 35 - resolved pr comments

--- app/src/components/SituationalQuestions.js
+++ app/src/components/SituationalQuestions.js
@@ -1,24 +1,35 @@
 import React, {useState} from 'react';
 import KnowYourRights from "./KnowYourRights";
 import Button from "@material-ui/core/Button";
-import {Box} from "@material-ui/core";
-
+import {Box, Grid} from "@material-ui/core";
 
 
 function SituationalQuestions() {
 
-  const [showUsers,setShowUsers]=useState(true)
-  function clickHandlerUsers(){
-    setShowUsers(false);
-  }
+    const [showUsers, setShowUsers] = useState(true)
+
+    function clickHandlerUsers() {
+        setShowUsers(false);
+    }
+
     return (
-    <Box component="span" m={1}>
-    {showUsers?
-      <Box mx={5} my={6}><Button variant="contained" color="secondary" onClick={clickHandlerUsers}>Community Member User</Button> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <Button variant="contained" color="secondary" onClick={clickHandlerUsers}>Targeted User</Button></Box>:
-        <KnowYourRights/>}
-    </Box>
+        <div>
+            {showUsers ? <Grid container spacing={1}
+  direction="row"
+  justifyContent="center"
+  alignItems="center" >
+                <Grid item >
+                    <Button variant="contained" color="secondary" onClick={clickHandlerUsers}>Community Member User</Button>
+                </Grid>
+                <Grid item>
+                    <Button variant="contained" color="secondary" onClick={clickHandlerUsers}>Targeted User</Button>
+                </Grid>
+                </Grid> : <KnowYourRights/>}
+
+        </div>
     )
-};
+}
+
+
 
 export default SituationalQuestions;
